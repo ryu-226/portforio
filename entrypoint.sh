c3800c8260846c0f1c6d0bf6ff8136daf27abfad
リュウ-226 <ryu226g@gmail.com>
2025-07-05 09:28:18 +0900

    Fix entrypoint.sh to create tmp/pids directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
 
+mkdir -p /app/tmp/pids
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
