4608aaa352003c495399c031c8720fb8d81fd8d2
リュウ-226 <ryu226g@gmail.com>
2025-07-12 18:13:09 +0900

    フォント変更

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['"M PLUS Rounded 1c"', ...defaultTheme.fontFamily.sans],
       },
     },
   },
