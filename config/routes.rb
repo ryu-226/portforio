f2332d3826fa210d203a5721fe2e3e047193b964
リュウ-226 <ryu226g@gmail.com>
2025-07-13 17:53:29 +0900

    ログインページ ログアウト機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root 'home#index'
+
   get 'signup', to: 'users#new', as: 'signup'
   post 'signup', to: 'users#create'
+
+  get 'login', to: 'sessions#new', as: 'login'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
+
 end
