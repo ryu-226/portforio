d1df77f9e171dfb8b359cac409eee3894fb86172
リュウ-226 <ryu226g@gmail.com>
2025-07-15 02:18:39 +0900

    設定ページ作成 仮

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,24 @@
 Rails.application.routes.draw do
-  get 'budgets/new'
+  root 'home#index'
+
+  get 'main', to: "main#index", as: "main"
+
+  get 'mypage', to: "mypage#index", as: "mypage"
+
   get 'budgets/create'
   get 'budgets/edit'
   get 'budgets/update'
+
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
-  root 'home#index'
 
   get 'signup', to: 'users#new', as: 'signup'
   post 'signup', to: 'users#create'
 
   get 'login', to: 'sessions#new', as: 'login'
   post 'login', to: 'sessions#create'
+
   delete 'logout', to: 'sessions#destroy', as: 'logout'
 
   resource :budget, only: [:new, :create, :edit, :update]
