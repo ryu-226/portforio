df6d6d8fb1ed4bc8fb0c782f42e85a52b487fcfe
リュウ-226 <ryu226g@gmail.com>
2025-07-16 20:02:05 +0900

    マイページ作成

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,4 +1,15 @@
 class MypageController < ApplicationController
+  before_action :require_login
+
   def index
+    @user = current_user
+    @budget = @user.budget
+    @member_days = (Time.zone.today - @user.created_at.in_time_zone('Asia/Tokyo').to_date).to_i    
+    
+    # ガチャ回数ダミー
+    @gacha_count = 0
+
+    # LINE通知ダミー
+    @line_status = "未連携"
   end
 end
