20267ff848267707ec51d21263593cee11b59d34
リュウ-226 <ryu226g@gmail.com>
2025-07-19 03:41:04 +0900

    メインページデザイン完成

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -8,6 +8,50 @@ class MainController < ApplicationController
       redirect_to new_budget_path, alert: "まずは予算を設定してください"
       return
     end
+
+    @low_messages = [
+      "セーブデー！賢く使おう",
+      "節約ランチでお得気分♪",
+      "ランチの工夫が腕の見せどころ！",
+      "今日はお財布にやさしく！",
+      "コスパ最強ランチを狙おう！"
+    ]
+    @middle_messages = [
+      "ランチタイムも楽しくガチャ！",
+      "ランチ仲間とシェアするのもおすすめ！",
+      "迷ったら定番メニュー！",
+      "おいしいランチでリフレッシュ！",
+      "バランスの良い食事を心がけよう！"
+    ]
+    @high_messages = [
+      "今日は豪華ランチいけるかも！",
+      "たまにはご褒美ランチも♪",
+      "今日はお気に入りのお店に行くチャンス！",
+      "ランチ仲間とプチ贅沢しよう！",
+      "美味しいもの食べて午後も頑張ろう！"
+    ]
+
+    @draw_message = nil
+
+    if @draw && @budget
+      min = @budget.min_amount
+      max = @budget.max_amount
+      range = max - min
+      twenty_percent = (range * 0.2).round
+
+      low_max = min + twenty_percent
+      high_min = max - twenty_percent + 1
+
+      amount = @draw.amount
+
+      if amount <= low_max
+        @draw_message = @low_messages.sample
+      elsif amount >= high_min
+        @draw_message = @high_messages.sample
+      else
+        @draw_message = @middle_messages.sample
+      end
+    end
   end
 
   def draw
