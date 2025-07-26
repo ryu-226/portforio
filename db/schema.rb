783c47a20f5a9979ec9822666211429d01f0ffa2
リュウ-226 <ryu226g@gmail.com>
2025-07-27 02:16:41 +0900

    Usersテーブルカラム削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_07_19_140541) do
+ActiveRecord::Schema[7.0].define(version: 2025_07_26_170949) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,8 +39,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_07_19_140541) do
     t.string "email", null: false
     t.string "password_digest", null: false
     t.string "nickname", null: false
-    t.string "line_uid"
-    t.boolean "line_notification_on", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
