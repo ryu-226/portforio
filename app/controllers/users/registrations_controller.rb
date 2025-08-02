55e360f0da6262990990c46a93f6af533f2d5ea3
リュウ-226 <ryu226g@gmail.com>
2025-08-03 01:18:26 +0900

    メソッドとビュー修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
-  # 新規登録後にリダイレクトするパスとメッセージを指定
   def after_sign_up_path_for(resource)
-    new_budget_path.tap do |path|
-      flash[:notice] = "会員登録が完了しました"
-    end
-  end
+    new_budget_path(from_signup: '1')
+  end  
 end
