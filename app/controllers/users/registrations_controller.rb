969cb05d3b012e507f4f12b218e17ea6b57c3070
リュウ-226 <ryu226g@gmail.com>
2025-08-04 07:39:58 +0900

    メールアドレス変更機能

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
-  def after_sign_up_path_for(resource)
-    new_budget_path(from_signup: '1')
-  end  
+  def after_inactive_sign_up_path_for(resource)
+    new_user_registration_path
+  end
 end
