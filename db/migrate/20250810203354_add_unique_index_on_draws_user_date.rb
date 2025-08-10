7d33f535c17af6bfa61c2f03f4e7c08c5aab3d70
リュウ-226 <ryu226g@gmail.com>
2025-08-11 06:02:58 +0900

    月集計を date の月範囲に統一

---
+++ db/migrate/20250810203354_add_unique_index_on_draws_user_date.rb
@@ -0,0 +1,12 @@
+class AddUniqueIndexOnDrawsUserDate < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+  def up
+    remove_index :draws, [:user_id, :date], if_exists: true
+    add_index :draws, [:user_id, :date], unique: true,
+              algorithm: :concurrently,
+              name: "index_draws_on_user_id_and_date_unique"
+  end
+  def down
+    remove_index :draws, name: "index_draws_on_user_id_and_date_unique", if_exists: true
+  end
+end
