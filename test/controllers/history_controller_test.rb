fca3cc3976ddb2d008875aadc6623cc630ff32f5
リュウ-226 <ryu226g@gmail.com>
2025-08-21 06:02:44 +0900

    Rubocop導入

--- test/controllers/history_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class HistoryControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get history_index_url
-    assert_response :success
-  end
-end
